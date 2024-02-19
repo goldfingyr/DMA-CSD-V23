9f013ac3453f23f0fcda7e77baeafc96bdeb6e3e
Karsten Jeppesen <kaje@ucn.dk>
2024-02-19 13:33:50 +0100

    SocketClient: Initial commit

---
+++ SocketServer/SocketClient/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SocketClient;
+
+Console.WriteLine("Starting Sender!");
+
+SocketSender theClient = new();
+theClient.DoSend(11000);
---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SocketServer;
+
+Console.WriteLine("Starting Server!");
+SocketListener socketListener = new();
+socketListener.DoListen(11000);
