4e19ad6dc0572cddcf2d85843d3d6e6bc7de0191
Karsten Jeppesen <kaje@ucn.dk>
2024-03-05 12:18:27 +0100

    CarAPI-3: Initial commit

---
+++ CarAPI-3/CarAPI/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace CarAPI.Models
+{
+    /// <summary>
+    /// Class Car goes with database table Cars goes with controller cars
+    /// </summary>
+    public class Car
+    {
+        public string licensplate { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+
+    }
+}
